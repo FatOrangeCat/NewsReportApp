49009cc021c9f216f442cead30ec14ef12910613
xlinwang <xlinwang2010@gmail.com>
2015-11-30 16:03:22 -0800

    checkin app

--- app/src/main/java/com/orangecat/android/newsreportapp/NewsActivity.java
+++ app/src/main/java/com/orangecat/android/newsreportapp/NewsActivity.java
@@ -1,13 +1,25 @@
 package com.orangecat.android.newsreportapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 
-public class NewsActivity extends AppCompatActivity {
+import java.util.UUID;
+
+public class NewsActivity extends SingleFragmentActivity {
+
+    private static final String EXTRA_NEWS_ID =
+            "com.orangecat.android.newsreportapp.news_id";
+
+    public static Intent newIntent(Context packageContext, UUID newsId){
+        Intent intent = new Intent(packageContext, NewsActivity.class);
+        intent.putExtra(EXTRA_NEWS_ID, newsId);
+        return intent;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
+    protected Fragment createFragment() {
+        UUID newsId = (UUID) getIntent().getSerializableExtra(EXTRA_NEWS_ID);
+        return NewsFragment.newInstance(newsId);
     }
 }
